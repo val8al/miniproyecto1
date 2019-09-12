6c42386036b1b7603bcdb1571877d68c7def1fcc
val8al <val8al@gmail.com>
2019-09-12 10:00:34 -0500

    dibujo base

--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@ void dibujaCuadro(){
     glColor3f(135.0/255.0,189.0/255.0,187.0/255.0);
     glBegin(GL_POLYGON);
     glVertex2f(394,138);
-    glVertex2f(618,138);
-    glVertex2f(618,363);
-    glVertex2f(393,363);
+    glVertex2f(621,138);
+    glVertex2f(621,364);
+    glVertex2f(393,364);
     glEnd();
 }
 void dibujaCirculo(float cx, float cy, float r, float num_segments){//hecho con la ayuda de https://stackoverflow.com/questions/22444450/drawing-circle-with-opengl
@@ -52,6 +52,12 @@ void draw(){
                                  {566.0,251.0},//3: gran circulo derecho
                                  {450.0,135.0},//4: medio circulo inferior izquierdo
                                  {567.0,135.0},//5: medio circulo inferior derecho
+                                 {450.0,368.0},//6: medio circulo superior izquierdo
+                                 {567.0,368.0},//7: medio circulo superior derecho
+                                 {390.0,313.0},//8: medio circulo lateral izquierdo superior
+                                 {390.0,198.0},//9: medio circulo lateral izquierdo inferior
+                                 {624.0,313.0},//10: medio circulo lateral derecho superior
+                                 {624.0,198.0},//11: medio circulo lateral derecho inferior
     };
     float radioCirculos = 62.0;
     float segmentosStdr = 100;
@@ -64,7 +70,12 @@ void draw(){
     dibujaCirculo(matrizDeDibujo[3][0],matrizDeDibujo[3][1],radioCirculos,segmentosStdr);//derecho
     dibujaMedioCirculo(matrizDeDibujo[4][0],matrizDeDibujo[4][1],radioCirculos,segmentosStdr,1.0,0);
     dibujaMedioCirculo(matrizDeDibujo[5][0],matrizDeDibujo[5][1],radioCirculos,segmentosStdr,1.0,0);
-
+    dibujaMedioCirculo(matrizDeDibujo[6][0],matrizDeDibujo[6][1],radioCirculos,segmentosStdr,-1.0,0);
+    dibujaMedioCirculo(matrizDeDibujo[7][0],matrizDeDibujo[7][1],radioCirculos,segmentosStdr,-1.0,0);
+    dibujaMedioCirculo(matrizDeDibujo[8][0],matrizDeDibujo[8][1],radioCirculos,segmentosStdr,1.0,1);
+    dibujaMedioCirculo(matrizDeDibujo[9][0],matrizDeDibujo[9][1],radioCirculos,segmentosStdr,1.0,1);
+    dibujaMedioCirculo(matrizDeDibujo[10][0],matrizDeDibujo[10][1],radioCirculos,segmentosStdr,-1.0,1);
+    dibujaMedioCirculo(matrizDeDibujo[11][0],matrizDeDibujo[11][1],radioCirculos,segmentosStdr,-1.0,1);
 
 
     glFlush();
